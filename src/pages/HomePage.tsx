4d99fb0eb73e4486af5f84677ffc309054bb35a7
Edwin Divine <omegbuedwin@gmail.com>
2024-09-18 06:31:45 +0100

    service section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import { MainLayout } from "@/layout/MainLayout"
+import { Services } from "@/sections/services"
 
 const HomePage = () => {
   return (
     <MainLayout>
-      <div>Home Page</div>
+      <div>
+        Home Page
+        <Services/>
+      </div>
     </MainLayout>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,10 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily: {
+			poppins:  '"Poppins" sans-serif',
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
