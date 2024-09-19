666e7526f4602057ff47a9060da6e04e64162958
Bilal <muilibilal@gmail.com>
2024-09-20 00:59:06 +0100

    feat: new arrival grid card done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
-import { MainLayout } from "@/layout/MainLayout"
+import Featured from "@/components/featured/featured";
+import { MainLayout } from "@/layout/MainLayout";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <div>Home Page</div>
+      <Featured />
     </MainLayout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
