e80e40d5a452a2619cdc74a8469f2d77a7c869a8
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-09-28 03:24:19 +0100

    repushing to git

--- src/components/cartTable/cartTable.tsx
+++ src/components/cartTable/cartTable.tsx
@@ -2,14 +2,14 @@
 import { Link } from "react-router-dom"
 const CartTable = () => {
     return (
-        <div className="bg-red-500 mx-auto md:w-[90%] max-w-[1170px] mt-5">
+        <div className="mx-auto md:w-[90%] max-w-[1170px] mt-5">
             <div className="flex items-center gap-2 md:w-[104px] p-1 ">
                 <Link to="/"><span className="text-[14px] cursor-pointer">Home</span></Link>
                 <span>/</span>
                 <span className="text-[14px]">Cart</span>
             </div>
             <div className="bg-green-400 md:mt-14">
-                <h2>hello</h2>
+                <h2>Table Section</h2>
 
             </div>
 
