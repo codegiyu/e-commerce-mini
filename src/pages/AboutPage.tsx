b0d2152f27737857373ac2e79418340513e72cdf
7Creed <4chimabika@gmail.com>
2024-10-01 12:49:37 +0100

    ft: slider component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,18 +1,22 @@
-import { OutlineBtn } from "@/components/atoms/OutlineBtn"
-import { RegularBtn } from "@/components/atoms/RegularBtn"
-import { SliderNavBtn } from "@/components/atoms/SliderNavBtn"
-import { MainLayout } from "@/layout/MainLayout"
+import Slider from "@/components/aboutSlider/Slider";
+import { OutlineBtn } from "@/components/atoms/OutlineBtn";
+import { RegularBtn } from "@/components/atoms/RegularBtn";
+import { SliderNavBtn } from "@/components/atoms/SliderNavBtn";
+import { MainLayout } from "@/layout/MainLayout";
+import { TeamLayout } from "@/layout/TeamLayout";
 
 const AboutPage = () => {
-    return (
-      <MainLayout>
-          <section className="container-main grid gap-6">
-            <RegularBtn text="View all products" />
-            <RegularBtn text="This is a full width button" fullWidth />
-            <SliderNavBtn direction="left" />
-            <OutlineBtn text="See all" />
-          </section>
-      </MainLayout>
-    )
-}
-export default AboutPage
+  return (
+    <TeamLayout>
+      <section className="container-main">
+        <div>About Page</div>
+        <RegularBtn text="View all products" />
+        <RegularBtn text="This is a full width button" fullWidth />
+        <Slider />
+        <SliderNavBtn direction="left" />
+        <OutlineBtn text="See all" />
+      </section>
+    </TeamLayout>
+  );
+};
+export default AboutPage;
