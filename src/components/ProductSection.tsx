7d8159311e08ff0162eead7debba49b22bd5741e
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-10-23 10:36:03 +0100

    work in progress

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -9,14 +9,14 @@ const ProductSection = () => {
     <div className="grid items-center container-main mb-5 ">
       <div className="grid  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-16 my-10">
         {productCard.map((item, idx) => (
-    <SingleCard  key={idx} id={String(idx)} img={item.image} name={item.name}  star={item.rating } isLiked={true} price={item?.price as number} color={item.color} isNew={item.isNew} discountPrice={item.discountPrice}/>
+          <SingleCard key={idx} id={String(idx)} img={item.image} name={item.name} star={item.rating} isLiked={true} price={item?.price as number} color={item.color} isNew={item.isNew} discountPrice={item.discountPrice} />
         ))}
       </div>
       <div className="flex mx-auto my-10">
         <Button className="capitalize bg-red-500  py-5 sm-md w-[250px] ">view all products</Button>
       </div>
 
-<CountDown/>
+      <CountDown />
 
     </div>
   );
